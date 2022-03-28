e88fd0ac4af6860453d6ff47d5f2a0d763323fc6
George Smyrlis <smirlagouras@gmail.com>
2022-03-28 21:29:14 +0300

    added pagerestaurant background image

--- HotelPtyxiaki/HotelPtyxiaki.Android/Resources/Resource.designer.cs
+++ HotelPtyxiaki/HotelPtyxiaki.Android/Resources/Resource.designer.cs
@@ -16691,22 +16691,25 @@ namespace HotelPtyxiaki.Droid
 			public const int restaurant144 = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int roomservice = 2131165337;
+			public const int restaurantbackground = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int test_custom_background = 2131165338;
+			public const int roomservice = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_dark = 2131165339;
+			public const int test_custom_background = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_light = 2131165340;
+			public const int tooltip_frame_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int TsamisHotel = 2131165341;
+			public const int tooltip_frame_light = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int xamarin_logo = 2131165342;
+			public const int TsamisHotel = 2131165342;
+			
+			// aapt resource value: 0x7F07009F
+			public const int xamarin_logo = 2131165343;
 			
 			static Drawable()
 			{
