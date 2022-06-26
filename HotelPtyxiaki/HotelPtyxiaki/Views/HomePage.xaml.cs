cc71033fde6c369eef2c2cc18396f981b9aabd0a
George Smyrlis <smirlagouras@gmail.com>
2022-06-26 18:16:42 +0300

    added homepage navigation, fixed some stuff and deleted up roomservice

--- HotelPtyxiaki/HotelPtyxiaki/Views/HomePage.xaml.cs
+++ HotelPtyxiaki/HotelPtyxiaki/Views/HomePage.xaml.cs
@@ -12,9 +12,31 @@ namespace HotelPtyxiaki.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        public IList<Page> Pages { get; private set; }
         public HomePage()
         {
             InitializeComponent();
+            
+        }
+
+        public async void ReceptionImageClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//PageReception");
+        }
+
+        public async void RestaurantImageClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//PageRestaurant");
+        }
+
+        public async void AboutImageClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//AboutPage");
+        }
+
+        public async void CleaningImageClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//PageCleaningService");
         }
     }
 }
