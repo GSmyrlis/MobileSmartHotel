7209dea513fc06ede37add88af510e4dbe1a7ef0
George Smyrlis <smirlagouras@gmail.com>
2022-07-01 16:26:05 +0300

    developing in reception started

--- HotelPtyxiaki/HotelPtyxiaki/Views/PageReception.xaml.cs
+++ HotelPtyxiaki/HotelPtyxiaki/Views/PageReception.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +13,7 @@ namespace HotelPtyxiaki.Views
     {
         public PageReception() 
         { 
-            InitializeComponent(); 
+            InitializeComponent();
         }
     }
 }
